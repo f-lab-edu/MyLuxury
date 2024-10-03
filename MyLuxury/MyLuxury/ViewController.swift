e180b909fad3fbbb79f136c9298f0e1201acaf96
goghrf <daynnite9924@gmail.com>
2024-10-03 18:24:25 +0900

    feat: 프로젝트 생성 및 초기 설정

---
+++ MyLuxury/MyLuxury/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyLuxury
+//
+//  Created by KoSungmin on 10/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+}
+
---
+++ MyLuxury/MyLuxuryTests/MyLuxuryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyLuxuryTests.swift
+//  MyLuxuryTests
+//
+//  Created by KoSungmin on 10/3/24.
+//
+
+import Testing
+@testable import MyLuxury
+
+struct MyLuxuryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
