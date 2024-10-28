1f5084d25f5b30391d87ed70667cfc97e151e138
goghrf <daynnite9924@gmail.com>
2024-10-28 16:02:22 +0900

    feat: 익스텐션 및 전역 상수 추가

---
+++ MyLuxury/Presentation/Sources/Presentation/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Presentation
+//
+//  Created by KoSungmin on 10/27/24.
+//
+
+import UIKit
+
+@MainActor let screenWidth = UIScreen.main.bounds.width
+@MainActor let screenHeight = UIScreen.main.bounds.height
+@MainActor let navigationBarHeight = screenHeight/14
