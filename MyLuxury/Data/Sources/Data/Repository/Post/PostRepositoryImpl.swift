709486caff9d75b702f623722deb7a7d91c2da8f
goghrf <daynnite9924@gmail.com>
2024-10-31 17:15:50 +0900

    feat: 홈 데이터 가져오기 기능 임시 구현 및 더미데이터 추가

--- MyLuxury/Data/Sources/Data/Repository/Post/PostRepositoryImpl.swift
+++ MyLuxury/Data/Sources/Data/Repository/Post/PostRepositoryImpl.swift
@@ -9,7 +9,32 @@ import Combine
 import Domain
 
 public class PostRepositoryImpl: PostRepository {
+   
     public init() {
         print("PostRepositoryImpl init")
+    }
+    
+    public func getHomeViewData() -> AnyPublisher<Domain.HomePostData, Never> {
+        let homePostData: HomePostData = (
+            todayPickPostData: Post(post_id: "123", postUIType: .normal, postCategory: .art, postTitle: "흑백 사진은 언제 처음 사용되었을까?", postThumbnailImage: "testImage1"),
+            newPostData: [
+                Post(post_id: "123", postUIType: .normal, postCategory: .art, postTitle: "흑백 사진은 언제 처음 사용되었을까?", postThumbnailImage: "testImage1"),
+                Post(post_id: "123", postUIType: .normal, postCategory: .art, postTitle: "흑백 사진은 언제 처음 사용되었을까?", postThumbnailImage: "testImage1")
+            ],
+            weeklyTopPostData: [
+                Post(post_id: "123", postUIType: .normal, postCategory: .art, postTitle: "흑백 사진은 언제 처음 사용되었을까?", postThumbnailImage: "testImage1"),
+                Post(post_id: "123", postUIType: .normal, postCategory: .art, postTitle: "흑백 사진은 언제 처음 사용되었을까?", postThumbnailImage: "testImage1")
+            ],
+            customizedPostData: [
+                Post(post_id: "123", postUIType: .normal, postCategory: .art, postTitle: "흑백 사진은 언제 처음 사용되었을까?", postThumbnailImage: "testImage1"),
+                Post(post_id: "123", postUIType: .normal, postCategory: .art, postTitle: "흑백 사진은 언제 처음 사용되었을까?", postThumbnailImage: "testImage1")
+            ],
+            editorRecommendationPostData: [
+                Post(post_id: "123", postUIType: .normal, postCategory: .art, postTitle: "흑백 사진은 언제 처음 사용되었을까?", postThumbnailImage: "testImage1"),
+                Post(post_id: "123", postUIType: .normal, postCategory: .art, postTitle: "흑백 사진은 언제 처음 사용되었을까?", postThumbnailImage: "testImage1")
+            ]
+        )
+        
+        return Just(homePostData).eraseToAnyPublisher()
     }
 }
--- MyLuxury/Domain/Sources/Domain/UseCase/PostUseCase.swift
+++ MyLuxury/Domain/Sources/Domain/UseCase/PostUseCase.swift
@@ -9,10 +9,14 @@ import Combine
 
 public protocol PostUseCase {
     var postRepository: PostRepository { get }
+    
+    func getHomeViewData() -> AnyPublisher<HomePostData, Never>
 }
 
 public class PostUseCaseImpl: PostUseCase {
+    
     public var postRepository: PostRepository
+    private var cancellables = Set<AnyCancellable>()
     
     public init(postRepository: PostRepository) {
         print("PostUseCase init")
@@ -21,5 +25,10 @@ public class PostUseCaseImpl: PostUseCase {
     
     deinit {
         print("PostUseCase deinit")
+    }
+    
+    public func getHomeViewData() -> AnyPublisher<HomePostData, Never> {
+
+        return postRepository.getHomeViewData().eraseToAnyPublisher()
     }
 }
