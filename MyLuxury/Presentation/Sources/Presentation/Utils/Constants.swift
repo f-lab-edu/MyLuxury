f7e85dd7836b2fea7394a24f5f5eecb11d1dee3c
goghrf <daynnite9924@gmail.com>
2024-10-31 16:12:36 +0900

    add: 홈 화면 뷰 길이 상수 정의

--- MyLuxury/Presentation/Sources/Presentation/Utils/Constants.swift
+++ MyLuxury/Presentation/Sources/Presentation/Utils/Constants.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+/// 디바이스 너비
 @MainActor let screenWidth = UIScreen.main.bounds.width
+/// 디바이스 높이
 @MainActor let screenHeight = UIScreen.main.bounds.height
+/// 상단 네비게이션바 높이
 @MainActor let navigationBarHeight = screenHeight/14
+/// 홈 화면 가로 컬렉션뷰 셀 변의 길이
+@MainActor let homeHorizontalCVCLength = screenWidth/3
+/// 홈 화면 2*2 그리드 컬렉션뷰 셀의 너비
+@MainActor let homeGridCVCWidth = (screenWidth-45)/2
+/// 홈 화면 2*2 그리드 컬렉션뷰 셀의 높이
+@MainActor let homeGridCVCHeight = homeGridCVCWidth / 1.77
+/// 홈 화면 에디터 추천 컬렉션 뷰 셀 변의 길이
+@MainActor let homeEditorRecommendCVCLength = screenWidth - 30
