5fabc718e6eaac99be93823581d6889e5fc17b53
goghrf <daynnite9924@gmail.com>
2024-10-31 17:11:02 +0900

    feat: 홈 코디네이터가 postUseCase 프로토콜에 의존성을 가지도록 처리

--- MyLuxury/Presentation/Sources/Presentation/Coordinator/HomeCoordinator.swift
+++ MyLuxury/Presentation/Sources/Presentation/Coordinator/HomeCoordinator.swift
@@ -6,18 +6,25 @@
 //
 
 import UIKit
+import Domain
 
 public protocol HomeCoordinator: Coordinator {
     var navigationController: UINavigationController { get set }
 }
 
+public protocol HomeCoordinatorDependency {
+    var postUseCase: PostUseCase { get }
+}
+
 public class HomeCoordinatorImpl: HomeCoordinator {
     public var navigationController: UINavigationController
     public var childCoordinators: [Coordinator] = []
+    public let dependency: HomeCoordinatorDependency
     
-    public init(navigationController: UINavigationController) {
+    public init(navigationController: UINavigationController, dependency: HomeCoordinatorDependency) {
         print("HomeCoordinatorImpl init")
         self.navigationController = navigationController
+        self.dependency = dependency
     }
     
     deinit {
@@ -25,10 +32,10 @@ public class HomeCoordinatorImpl: HomeCoordinator {
     }
     
     public func start() {
-        let homeVM = HomeViewModel()
+        let homeVM = HomeViewModel(postUseCase: self.dependency.postUseCase)
         let homeVC = HomeViewController(homeVM: homeVM)
         self.navigationController = UINavigationController(rootViewController: homeVC)
         self.navigationController.isNavigationBarHidden = true
-        homeVC.tabBarItem = UITabBarItem(title: TabBarItem.home.title, image: UIImage(systemName: TabBarItem.home.image), tag: 0)
+        homeVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabBarItem.home.image)?.withTintColor(.gray, renderingMode: .alwaysOriginal), selectedImage: UIImage(systemName: TabBarItem.home.image)?.withTintColor(.white, renderingMode: .alwaysOriginal))
     }
 }
