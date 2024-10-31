838cfc68e6ca3e837496a0ba8ad94e42a3a3f039
goghrf <daynnite9924@gmail.com>
2024-10-31 17:02:01 +0900

    add: PostViewController 초기 파일 추가

---
+++ MyLuxury/Presentation/Sources/Presentation/Post/ViewController/PostViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PostViewController.swift
+//  Presentation
+//
+//  Created by KoSungmin on 10/29/24.
+//
+
+import UIKit
+
+final class PostViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
