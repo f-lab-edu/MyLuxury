d23282fd1abd513bf5a7dd8c65d7eb4d555f36a8
goghrf <daynnite9924@gmail.com>
2024-10-31 16:57:30 +0900

    feat: 홈 컨텐츠뷰 구현

---
+++ MyLuxury/Presentation/Sources/Presentation/Home/View/HomeContents/HomeContentsView.swift
@@ -0,0 +1,106 @@
+//
+//  HomeContentsView.swift
+//  Presentation
+//
+//  Created by KoSungmin on 10/28/24.
+//
+
+import UIKit
+
+final class HomeContentsView: UIView {
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 30
+        return stackView
+    }()
+    
+    let homeTodayPickView: HomeTodayPickView = {
+        let view = HomeTodayPickView()
+        return view
+    }()
+    
+    let newPostsCV: HomeHorizontalCollectionView = {
+        let cv = HomeHorizontalCollectionView()
+        cv.title = "새로 게시된 지식"
+        return cv
+    }()
+    
+    let weeklyTopPostsCV: HomeHorizontalCollectionView = {
+        let cv = HomeHorizontalCollectionView()
+        cv.title = "이번 주 TOP10"
+        return cv
+    }()
+    
+    let homeGridCV: HomeGridCollectionView = {
+        let cv = HomeGridCollectionView()
+        cv.title = "회원님이 좋아할 만한"
+        return cv
+    }()
+    
+    let homeEditorRecommendCV: HomeEditorRecommendCollectionView = {
+        let cv = HomeEditorRecommendCollectionView()
+        cv.title = "에디터 추천 지식"
+        return cv
+    }()
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpHierarchy()
+        setUpLayout()
+        scrollView.backgroundColor = .black
+        stackView.backgroundColor = .clear
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpHierarchy() {
+        self.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        stackView.addArrangedSubview(homeTodayPickView)
+        stackView.addArrangedSubview(newPostsCV)
+        stackView.addArrangedSubview(weeklyTopPostsCV)
+        stackView.addArrangedSubview(homeGridCV)
+        stackView.addArrangedSubview(homeEditorRecommendCV)
+    }
+    
+    private func setUpLayout() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        homeTodayPickView.translatesAutoresizingMaskIntoConstraints = false
+        newPostsCV.translatesAutoresizingMaskIntoConstraints = false
+        weeklyTopPostsCV.translatesAutoresizingMaskIntoConstraints = false
+        homeGridCV.translatesAutoresizingMaskIntoConstraints = false
+        homeEditorRecommendCV.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            homeTodayPickView.topAnchor.constraint(equalTo: stackView.topAnchor),
+            homeTodayPickView.centerXAnchor.constraint(equalTo: stackView.centerXAnchor),
+            newPostsCV.topAnchor.constraint(equalTo: homeTodayPickView.bottomAnchor, constant: 30),
+            weeklyTopPostsCV.topAnchor.constraint(equalTo: newPostsCV.bottomAnchor, constant: 30),
+            homeGridCV.topAnchor.constraint(equalTo: weeklyTopPostsCV.bottomAnchor, constant: 30),
+            homeGridCV.centerXAnchor.constraint(equalTo: stackView.centerXAnchor),
+            homeEditorRecommendCV.topAnchor.constraint(equalTo: homeGridCV.bottomAnchor, constant: 30),
+            homeEditorRecommendCV.centerXAnchor.constraint(equalTo: stackView.centerXAnchor)
+        ])
+    }
+}
