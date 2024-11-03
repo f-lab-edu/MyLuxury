9b29d8ad8ccac0c18a48a5c550330d8eebc126e1
goghrf <daynnite9924@gmail.com>
2024-11-04 02:23:54 +0900

    feat: Post Presentation 객체 초기 생성

---
+++ MyLuxury/Presentation/Sources/Presentation/Post/View/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  Presentation
+//
+//  Created by KoSungmin on 11/4/24.
+//
+
+import UIKit
+
+final class PostView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .green
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- MyLuxury/Presentation/Sources/Presentation/Post/ViewController/PostViewController.swift
+++ MyLuxury/Presentation/Sources/Presentation/Post/ViewController/PostViewController.swift
@@ -8,9 +8,28 @@
 import UIKit
 
 final class PostViewController: UIViewController {
+    let rootView = PostView()
+    let postVM: PostViewModel
+    
+    init(postVM: PostViewModel) {
+        print("PostViewController init")
+        self.postVM = postVM
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        print("PostViewController deinit")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+    }
+    
+    override func loadView() {
+        self.view = rootView
     }
 }
