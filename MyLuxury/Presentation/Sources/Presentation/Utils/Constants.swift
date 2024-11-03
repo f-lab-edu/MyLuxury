8d149db338e638b0c58985fcb406f5d80ec32bbc
goghrf <daynnite9924@gmail.com>
2024-11-04 02:56:19 +0900

    add: 홈 오늘의 픽뷰 길이 상수 추가

--- MyLuxury/Presentation/Sources/Presentation/Utils/Constants.swift
+++ MyLuxury/Presentation/Sources/Presentation/Utils/Constants.swift
@@ -13,6 +13,10 @@ import UIKit
 @MainActor let screenHeight = UIScreen.main.bounds.height
 /// 상단 네비게이션바 높이
 @MainActor let navigationBarHeight = screenHeight/14
+/// 홈 화면 오늘의 픽뷰의 너비
+@MainActor let hometodayPickViewWidth = screenWidth - 30
+/// 홈 화면 오늘의 픽뷰의 높이
+@MainActor let hometodayPickViewHeight = hometodayPickViewWidth/2.5
 /// 홈 화면 가로 컬렉션뷰 셀 변의 길이
 @MainActor let homeHorizontalCVCLength = screenWidth/3
 /// 홈 화면 2*2 그리드 컬렉션뷰 셀의 너비
